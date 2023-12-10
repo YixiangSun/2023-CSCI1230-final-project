6da2f447a06ec9718b78934ca83e190134a3bc1c
Julia Fu <wanjia_fu@brown.edu>
2023-12-09 21:20:37 -0500

    reduce range of fire

--- src/realtime.cpp
+++ src/realtime.cpp
@@ -354,19 +354,19 @@ void Realtime::draw(RenderShapeData& shape, bool ifBall, glm::mat4 originalCTM) 
             float y_vel = 0.1f;
             glm::vec3 velocity;
             if (shape.riseCount == 0){
-                shape.timeOffset = ((double) rand() / (RAND_MAX)) * 80.f;
+                shape.timeOffset = ((double) rand() / (RAND_MAX)) * 60.f;
                 shape.ctm = shape.ctm * glm::translate(glm::mat4(1.f), glm::vec3(0, shape.timeOffset * y_vel, 0));
             }
             shape.riseCount += 1;
-            float r = ((double) rand() / (RAND_MAX)) * 0.2f;
-            if (shape.riseCount <= 40){
+            float r = ((double) rand() / (RAND_MAX)) * 0.04f;
+            if (shape.riseCount <= 30){
                 velocity = glm::vec3(r * xz_vel[0], (double) rand() / (RAND_MAX) * y_vel, r * xz_vel[1]);
-            }else if(shape.riseCount <= 90){
+            }else if(shape.riseCount <= 80){
                 velocity = glm::vec3(0.f, (double) rand() / (RAND_MAX) * y_vel, 0.f);
             }else{
                 velocity = glm::vec3(-r * xz_vel[0], (double) rand() / (RAND_MAX) * y_vel, -r * xz_vel[1]);
             }
-            if (shape.riseCount + shape.timeOffset >= 119.f){
+            if (shape.riseCount + shape.timeOffset >= 99.f){
                 shape.ctm = originalCTM;
                 shape.primitive.material.cAmbient[1] = 0.0f;
                 shape.riseCount = 0;
