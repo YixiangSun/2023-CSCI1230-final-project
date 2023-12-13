cbc54067933db09c2d196a6e5f5ff002ed1010dc
Julia Fu <wanjia_fu@brown.edu>
2023-12-13 14:56:44 -0500

    fire location

--- src/realtime.cpp
+++ src/realtime.cpp
@@ -193,7 +193,7 @@ void Realtime::initializeGL() {
     Realtime::makeFBO();
 
     initialized = true;
-    m_fire_center = glm::vec3(2.1, 0.1, 1.2);
+    m_fire_center = glm::vec3(0.65, 0.1, 4.2);
 }
 
 void Realtime::readTexture() {
